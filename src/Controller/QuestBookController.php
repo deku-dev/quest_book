e7453a5770ea830bdab306a7179e440bd1c234c7
Deku-dev <gurycvadym510@gmail.com>
2021-10-26 14:54:59 +0300

    Add table and get data from database

--- src/Controller/QuestBookController.php
+++ src/Controller/QuestBookController.php
@@ -10,16 +10,44 @@ use Drupal\Core\Controller\ControllerBase;
 class QuestBookController extends ControllerBase {
 
   /**
-   * Builds the response.
+   * Build page quest book with form, all reviews, edit button.
    */
-  public function build() {
+  public function buildQuestBook() {
 
-    $build['content'] = [
-      '#type' => 'item',
-      '#markup' => $this->t('It works!'),
+    $review = $this->getReviews();
+    $build['reviews'] = [
+      '#type' => 'table',
+      '#header' => [
+        $this->t('Name'),
+        $this->t('Phone'),
+        $this->t('Email'),
+        $this->t('Text'),
+        $this->t('Date'),
+      ],
     ];
-
+    while ($result = $review->fetchAssoc()) {
+      $build['reviews']['#rows'][$result['id']] = [
+        'id' => '1',
+        'name' => '1',
+        'email' => '2',
+        'tel_number' => '4',
+        'date' => '6',
+      ];
+    }
     return $build;
+  }
+
+  /**
+   * Get from database data for reviews page.
+   */
+  private function getReviews() {
+    $database = \Drupal::service('database');
+
+    return $database
+      ->select('quest_book', 'qb')
+      ->fields('qb', ['name', 'tel_number', 'email', 'review_text', 'created'])
+      ->orderBy('created')
+      ->execute();
   }
 
 }
